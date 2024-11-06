7a697879c37db649fba03812089a62b76dd66b7b
Hans Llanos <hans.llanos@gmail.com>
2024-11-05 19:41:13 -0500

    proyecto base

---
+++ kaftka-schema-registry/src/main/java/com/example/kaftka_schema_registry/KaftkaSchemaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaftka_schema_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaftkaSchemaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaftkaSchemaRegistryApplication.class, args);
+	}
+
+}
---
+++ kaftka-schema-registry/src/test/java/com/example/kaftka_schema_registry/KaftkaSchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaftka_schema_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaftkaSchemaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
